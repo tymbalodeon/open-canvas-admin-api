bc876e6821c2e845be3c502d007ea35043122d7d
Ben Rosen <benjamin.j.rosen@gmail.com>
2022-01-26 15:30:52 -0500

    update sync courses

--- open_canvas/canvas/sync.py
+++ open_canvas/canvas/sync.py
@@ -1,2 +1,16 @@
-def sync_courses():
-    pass
+from canvas.api import get_canvas
+from canvas.constants import ACCOUNT
+
+from open_canvas.models import CanvasSite
+
+
+def sync_courses(test=False, account=ACCOUNT):
+    account = get_canvas(test).get_account(account)
+    courses = account.get_courses()
+    for course in courses:
+        CanvasSite.objects.create(
+            canvas_id=course.id,
+            name=course.name,
+            course_code=course.sis_course_id,
+            workflow_state=course.workflow_state,
+        )
